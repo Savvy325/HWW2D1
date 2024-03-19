4823a8ef89c99bcebb23afd5b790d0cf387e53e4
Savvy325 <vannahlyles@yahoo.com>
2024-03-19 12:54:21 -0500

    initial commit

---
+++ homework.py
@@ -0,0 +1,66 @@
+# 1. Showcase Your Dance Moves!
+
+# Task 1: Code Correction
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take umbrella!")
+
+# Task 2: Your Mood Today
+mood = "happy"
+if mood == "happy":
+    print("That's great to hear!")
+if mood == "sad":
+    print("I hope your day gets better!")
+
+# 2. Python Naming Convention Practice
+pi_value = 3.14
+print(pi_value)
+user_age = 25
+print(user_age)
+user_location = "New York"
+print(user_location)
+max_limit = 1000
+print(max_limit)
+
+#3. Python Data Types and type() Function
+variable_a = "Hello, World!"
+print(variable_a)
+#variable a is a str
+
+variable_b = 23
+print(variable_b)
+#variable b is an integer
+
+variable_c = 3.14
+print(variable_c)
+#variable c is a floating point
+
+variable_d = True
+print(variable_d)
+# variable d is a boolean
+
+#4. Arithmetic Operations in Daily Life
+
+#Task 1: Grocery Store Math
+bread = 3
+peanut_butter = 5
+jelly = 4
+sandwich = bread + peanut_butter + jelly
+print(sandwich)
+#Task 2: Bank Interest
+initial_amount = 5000
+interest = .03
+bank = initial_amount * interest
+balance = initial_amount + bank
+print(balance)
+
+#5 Understanding Assignments and Comparisons
+x = 15
+y = 30
+x, y = y, x
+print("Value of x: ", x)
+print("Value of y: ", y)
+
+
